0a51456b62e8abf511b9b0f2beb56387d0d4b413
Uturi <sheep627@naver.com>
2019-03-07 16:24:52 +0900

    산술연산 실습

---
+++ OperTest2.java
@@ -0,0 +1,28 @@
+public class OperTest2 
+{
+	public static void main(String[] args) 
+	{
+		int num1 = 13, num2 = 5, num3 = num1 + num2; 		
+
+		System.out.println(num1+" + "+num2+" = "+num3);
+		System.out.println(num1+" + "+num2+" = "+num1+num2);
+		System.out.println(num1+" + "+num2+" = "+(num1+num2));
+
+		num3 = num1 - num2;
+		System.out.println(num1+" - "+num2+" = "+num3);
+		System.out.println(num1+" + "+num2+" = "+(num1-num2));
+
+		num3 = num1 * num2;
+		System.out.println(num1+" * "+num2+" = "+num3);
+		System.out.println(num1+" + "+num2+" = "+num1*num2);
+
+		num3 = num1 / num2;
+		System.out.println(num1+" / "+num2+" = "+num3);
+		System.out.println(num1+" + "+num2+" = "+num1/num2);
+
+		num3 = num1 % num2;
+		System.out.println(num1+" % "+num2+" = "+num3);
+		System.out.println(num1+" + "+num2+" = "+num1%num2);
+	}
+}
+
